0eb5097f875e2bec46e5ae063d6d51bc4fb4da9b
CicadaMusic <team@cicadamusic.net>
2025-12-15 12:21:28 +0200

    changed paly

--- play.js
+++ play.js
@@ -1,11 +1,30 @@
-function getNumbers() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve([1, 2, 3, 4, 5]);
-    }, 700);
-  });
-}
+// function getNumbers(numbers) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (numbers.length === 0) {
+//         reject(new Error("No numbers provided"));
+//         return;
+//       }
+//       resolve(numbers);
+//     }, 1000);
+//   });
+// }
 
-getNumbers().then((resolve) =>
-  resolve.forEach((number) => console.log(number))
-);
+// getNumbers([])
+//   .then((data) =>
+//     data.forEach((number) => {
+//       console.log(number);
+//     })
+//   )
+//   .catch((error) => console.error(error));
+
+fetch("https://swapi.dev/api/people/3")
+  .then((data) => {
+    return data.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.error("You have an error\n", err));
+
+console.log("my first message")
